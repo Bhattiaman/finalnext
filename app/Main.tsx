2df6809309a12e9d8d0aba1d665f83a4f26a40e4
Bhattiaman <bhattiaman@kali.Bhattiaman>
2023-08-31 15:28:31 +0530

    cisco update

--- app/Main.tsx
+++ app/Main.tsx
@@ -86,6 +86,9 @@ export default function Home({ posts }) {
           <NewsletterForm />
         </div>
       )}
+      
+        
+
     </>
   )
 }
--- app/head.tsx
+++ app/head.tsx
@@ -1,6 +1,6 @@
 export default function Head() {
   return (
-    <>
+    <>  
       <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
@@ -11,6 +11,8 @@ export default function Head() {
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      
+     
     </>
   )
 }
